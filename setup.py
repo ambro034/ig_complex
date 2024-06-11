4d1ed942a9dce48b73f2296aeed8c23f3003aa0c
ambro034 <123208997+ambro034@users.noreply.github.com>
2024-06-10 20:53:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-  name = 'igcomplex',
+  name = 'ig_complex',
   version = '0.0.1',
   description = "A package used to identify complexity in policy statements",
   url = 'https://github.com/ambro034/text_reuse.git',
   author_name = 'Graham W. Ambrose',
   license = 'unlicense',
-  package = ['igcomplex'],
+  package = ['ig_complex'],
   install_requires=['spacy',
                    'svglib',
                    'pandas',
